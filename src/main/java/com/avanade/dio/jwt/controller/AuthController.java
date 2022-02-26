6f4c11802a579aadb13fd0be98e5645441da1571
allysomoliveira <allysomoliveira7@gmail.com>
2022-02-26 00:13:15 -0300

    first commit

---
+++ src/main/java/com/avanade/dio/jwt/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.avanade.dio.jwt.controller;
+
+import com.avanade.dio.jwt.data.UserData;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthController {
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public AuthController(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/login")
+    public void login(@RequestBody UserData user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+    }
+
+}
